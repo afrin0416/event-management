0939377811cc287910833a5e9eb6a551e0078aca
Afroza Akter Afrin <afrozaafrin124142@gmail.com>
2025-08-10 01:27:21 +0600

    route change to base from '/events'

--- event/urls.py
+++ event/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from event import views
 
 urlpatterns = [
-    path('events/', views.event_list, name='event_list'),
+    path('', views.event_list, name='event_list'),
     path('events/<int:event_id>/', views.event_detail, name='event_detail'),
     path('events/create/', views.event_create, name='event_create'),
     path('dashboard/', views.organizer_dashboard, name='dashboard'),
